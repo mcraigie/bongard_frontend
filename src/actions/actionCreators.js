8f664e8f4cd977b44149d1aa01d0ebe286fb590e
mcraigie <max.craigie@gmail.com>
2018-01-02 13:39:39 +1100

    remove scoring

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,32 +1,26 @@
-export function incrementScore() {
-  return {
-    type: 'INCREMENT_SCORE',
-  };
-}
-
 export function setAsTouchDevice() {
   return {
-    type: 'SET_AS_TOUCH_DEVICE',
+    type: "SET_AS_TOUCH_DEVICE",
   };
 }
 
 export function setCurrentProblem(problem) {
   return {
-    type: 'SET_CURRENT_PROBLEM',
+    type: "SET_CURRENT_PROBLEM",
     problem,
   };
 }
 
 export function setError(error) {
   return {
-    type: 'SET_ERROR',
+    type: "SET_ERROR",
     error,
   };
 }
 
 export function respond(problem, answer) {
   return {
-    type: 'RESPOND',
+    type: "RESPOND",
     problem,
     answer,
   };
@@ -34,6 +28,6 @@ export function respond(problem, answer) {
 
 export function toggleInstructions() {
   return {
-    type: 'TOGGLE_INSTRUCTIONS',
+    type: "TOGGLE_INSTRUCTIONS",
   };
 }
--- src/reducers/score.js
+++
@@ -1,9 +0,0 @@
-function score(state = 0, action) {
-  switch (action.type) {
-    case 'RESPOND':
-      return state + (action.problem.correctAnswerId === action.answer ? 100 : 0);
-    default: return state;
-  }
-}
-
-export default score;
