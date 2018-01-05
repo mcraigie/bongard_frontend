9ceb6aad5bfc883d1df8b5601efcb751e72ab5f2
mcraigie <max.craigie@gmail.com>
2018-01-05 18:32:11 +1100

    simplify action type definitions

--- src/types/actions.d.ts
+++ src/types/actions.d.ts
@@ -1,30 +1,20 @@
-type SET_PROBLEM = "SET_PROBLEM";
-type RESPOND = "RESPOND";
-type TOGGLE_INSTRUCTIONS = "TOGGLE_INSTRUCTIONS";
-type LOCATION_CHANGE = "@@router/LOCATION_CHANGE";
-type OTHER_ACTION = "__any_other_action_type__";
-
 interface ProblemAction {
-  type: SET_PROBLEM;
+  type: "SET_PROBLEM";
   problem: Problem;
 }
 
 interface ResponseAction {
-  type: RESPOND;
+  type: "RESPOND";
   problem: Problem;
   answer: string;
 }
 
 interface ToggleAction {
-  type: TOGGLE_INSTRUCTIONS;
+  type: "TOGGLE_INSTRUCTIONS";
 }
 
 interface LocationAction {
-  type: LOCATION_CHANGE;
+  type: "@@router/LOCATION_CHANGE";
 }
 
-interface OtherAction {
-  type: OTHER_ACTION;
-}
-
-type ActionTypes = ProblemAction | ResponseAction | ToggleAction | LocationAction | OtherAction;
+type ActionTypes = ProblemAction | ResponseAction | ToggleAction | LocationAction;
